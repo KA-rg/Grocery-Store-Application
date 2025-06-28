2be7696155151ddc87135abf6d883afb55f3a455
KA-rg <krishjain260905@gmail.com>
2025-06-28 19:00:16 +0530

    ui made

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,9 +1,11 @@
+import datetime
 import mysql.connector
 
 __cnx = None
 
-
 def get_sql_connection():
+  print("Opening mysql connection")
+  global __cnx
 
   __cnx = mysql.connector.connect(user='root', password='Krish@2005',
                                 host='127.0.0.1',
