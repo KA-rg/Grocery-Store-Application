bf993a5b976ea7c957196a1186654dd45c0bb2a2
KA-rg <krishjain260905@gmail.com>
2025-06-28 12:31:19 +0530

    backend connected with sql

---
+++ backend.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='root', password='Krish@2005',
+                              host='127.0.0.1',
+                              database='gs')
+
+
+
+
+
+
+
+
+
+
+cnx.close()
